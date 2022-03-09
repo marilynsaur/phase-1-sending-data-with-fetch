26e3da0a283057274c63c66a7081d5989190dada
Marilyn Wren <Marilynwren64@yahoo.com>
2022-03-09 17:11:37 -0500

    commit tree bd72f00bb9ecaf996a27710595b651139099a215

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    });
+
+    
+
+
+function submitData(name, email) {
+    const data = {
+        name: name,
+        email: email,
+    };
+    
+
+    const configurationObject2 = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+    
+  return fetch("http://localhost:3000/users", configurationObject2)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+           
+            document.body.innerHTML = object.id
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            console.log(error.message);
+            document.body.innerHTML = error
+        });
+   
+}
